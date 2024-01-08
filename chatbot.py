6d258d6115074a8073478f9209910aa88228ea7f
Berat Cabuk <102920898+beratcabuk@users.noreply.github.com>
2024-01-08 13:13:15 +0300

    refactored code, added chatbot and driver classes

---
+++ chatbot.py
@@ -0,0 +1,35 @@
+from openai import OpenAI
+import json
+
+API_KEY = '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'
+
+class Bot:
+    def __init__(self, model:str, user_full_name:str) -> None:
+        self.client = OpenAI(api_key=API_KEY)
+        self.model = model
+        self.user_full_name = user_full_name
+    
+    def respond(self, payload:str, objective:str) -> str:
+        response = self.client.chat.completions.create(
+            model=self.model,
+            response_format={ "type": "json_object" },
+            messages=[
+                {"role": "system", "content": f"""You are a helpful assistant designed to output JSON.
+                                                The output should just include the response message, no additional data.
+                                                Use the 'message' tag for the JSON object. You are designed to act as my secretary,
+                                                respond to the incoming messages on my behalf. Do not use any emojis or special characters.
+                                                The objective of this conversation is this: {objective}.
+                                                The messages you will get will be in a fixed format: 
+                                                [HH:MM, DD/MM/YYYY] The name of the sender: The message. If the message is a 
+                                                reply to another, it's in the following format: [HH:MM, DD/MM/YYYY] The name of the sender: The name of the 
+                                                person being replied to\\nThe message being replied to\\nThe reply. My name is {self.user_full_name}, any other name
+                                                you see refers to the contact I am messaging. You will be provided with the last 10 messages for context.
+                                                Please converse with the contact on my behalf by returning a response message as if you are me.
+                                                Once the objective is reached, but you have to send a final message, return *123# at the end of the message.
+                                                If the objective is reached, but you do not have to send another message, just return *123#."""},
+                {"role": "user", "content": payload}
+            ]
+            )
+        msg = json.loads(response.choices[0].message.content)['message']
+
+        return msg
---
+++ driver.py
@@ -0,0 +1,60 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+WEBSITE_URL = 'https://web.whatsapp.com/'
+
+class Driver:
+    def __init__(self) -> None:
+        self.driver = webdriver.Firefox()
+        self.driver.get(WEBSITE_URL)
+        sleep(10)
+
+    def save_qr(self) -> None:
+        self.qr_element = self.driver.find_element(By.CLASS_NAME, '_19vUU')
+        self.qr_element.screenshot(filename='qr.png')
+    
+    def find_contact(self, contact_name: str) -> None:
+        self.contact_name = contact_name
+        self.search_box = self.driver.find_element(By.CSS_SELECTOR, '.qh0vvdkp')
+        self.search_box.click()
+        sleep(1)
+
+        for i in range(len(self.contact_name)):
+            self.search_box.send_keys(self.contact_name[i])
+            sleep(0.1)
+        self.search_box.send_keys(Keys.ENTER)
+    
+    def scroll_up(self) -> None:
+        for i in range(5):
+            self.driver.find_element(By.CSS_SELECTOR, '.n5hs2j7m').send_keys(Keys.PAGE_UP)
+            sleep(0.5)
+
+    def fetch_messaging_history(self, n_last_messages:int = 10) -> (str, bool):
+        self.visible_msgs = self.driver.find_elements(By.CLASS_NAME, 'cm280p3y.to2l77zo.n1yiu2zv.c6f98ldp.ooty25bp.oq31bsqd')
+        last_n = []
+        for i in range(1, n_last_messages + 1):
+            msg = self.visible_msgs[-i].find_element(By.TAG_NAME, 'div')
+            msg_text = msg.text
+            msg_metadata = msg.get_attribute('data-pre-plain-text')
+            last_n.append(msg_metadata + msg_text)
+        last_n_messages = ''.join(last_n[::-1])
+
+        # If the last message is not from the contact, we sent it.
+        sent_last_message = bool(last_n[0].split(' ')[2][:-1] != self.contact_name)
+        return (last_n_messages, sent_last_message)
+
+    def send_message(self, payload: str):
+        input_box = self.driver.find_element(By.CSS_SELECTOR, '._3Uu1_ > div:nth-child(1) > div:nth-child(1)')
+        sleep(1)
+        input_box.click()
+        sleep(1)
+        for i in range(len(payload)):
+            input_box.send_keys(payload[i])
+            sleep(0.1)
+        input_box.send_keys(Keys.ENTER)
+
+    def quit(self):
+        sleep(0.5)
+        self.driver.quit()
